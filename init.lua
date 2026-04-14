347a8f1ae6c1f455d5efc0a829db7e7f046f3956
Jeremiahtaylor2017 <37142633+Jeremiahtaylor2017@users.noreply.github.com>
2026-04-13 22:56:23 -0700

    force 4 space tabs on save

--- init.lua
+++ init.lua
@@ -224,6 +224,12 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Format on save for files not using tab as 4 spaces
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  command = 'normal! gg=G',
+})
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
